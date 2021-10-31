719aa7d67ec9be2fafd9c362af0e1283756481ce
Jen <j.hofbauer223@t-online.de>
2021-11-01 00:26:39 +0100

    :lipstick: Changed style to terminal

--- pages/index.js
+++ pages/index.js
@@ -20,79 +20,85 @@ export default function Home ({ hCaptchaSiteKey }) {
   const [verified, setVerified] = useState(false)
 
   return (
-    <Container className={[styles.container, 'p-3']}>
-      <Header />
-      <p>
-        Am 11.12.2021 veranstalten wir einen Capture The Flag (CTF). Ein CTF ist ein Wettbewerb bei dem
-        man legal {'"hacken"'} kann, also sein Können im Bereich IT-Sicherheit, Forensik und co. unter Beweis
-        stellen kann.
-        <br />
-        <a href="https://de.wikipedia.org/wiki/Capture_the_Flag#Computersicherheit">
-          Mehr Informationen zu CTFs
-        </a>
-      </p>
-      <p>
-        Unser CTF ist Dabei so gestaltet dass auch Menschen mit wenig Erfahrung mitmachen können.
-        Es gibt zu jeder Kategorie jeweils eine leichte, eine mittlere und eine schwere Aufgabe.
-        <br />
-        <b>Kategorien</b>: web, stego, forensic, crypto, osint, re, pwn
-      </p>
-      <p>
-        Teams sind erlaubt, Preise gibt es jedoch nur für die besten drei Einzelkämpfer.
-        <br />
-        <b>Preise</b>: Hack The Box Gift Cards
-      </p>
-
-      <h2>Eckdaten</h2>
-      <p>
-        <b>Datum</b>: 11.12.2021<br />
-        <b>Uhrzeit</b>: 10 bis 18 Uhr<br />
-        <b>Ort</b>: Technische Hochschule Ingolstadt<br />
-        <b>Raum</b>: TBD<br />
-        <b>Teilnehmer</b>: ca. 20-40 einzeln oder in Teams ohne Wertung<br />
-      </p>
-
-      <h2>Sponsoren</h2>
-      <p>
-        folgen
-      </p>
-
-      <h2>Anmeldung</h2>
-      <Form method="post" action="api/submit">
-        <Form.Group className="mb-3">
-          <Form.Label>Name / Nickname</Form.Label>
-          <Form.Control
-            name="name"
-            type="text"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>E-Mail-Adresse:</Form.Label>
-          <Form.Control
-            name="email"
-            type="email"
-            required
-            />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Studiengang:</Form.Label>
-          <Form.Control
-            name="course"
-            type="text"
-            placeholder="Informatik 3. Semester, Bachelorand Künstliche Intelligenz, ..."
-            />
-        </Form.Group>
-        <Form.Group>
-          <HCaptcha
-            sitekey={hCaptchaSiteKey}
-            onVerify={() => setVerified(true)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" disabled={!verified}>Absenden</Button>
-      </Form>
-      <Footer />
-    </Container>
+        <Container className={[styles.container, 'p-3']}>
+            <br></br> <br></br>
+            <Header/>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -info
+                <br></br>
+                Capture the Flag ist ein Wettbewerb bei dem man legal hacken kann, du wirst Aufgaben in der Kategorien
+                web, stego, forensic, crypto, osint, re und pwn lösen.
+                Für jeden richtigen Hack gibt es eine Flage, die dir Punkte und einen Platz auf dem Scoreboard sichert.
+                Hierfür benötigt ihr keine Vorkenntnisse, der CTF beinhaltet Aufgaben für alle Erfahrungsgerade.
+                Neben dem Kennenlernen von Studierenden mit gleichen Interessen und dem Spaß des beim Rätsel lösen gibt
+                es natürlich auch Essen/Trinken und Preise für die besten Hacker.
+            </p>
+            <br></br>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -wann
+                <br></br>
+                11.12.2021 von 10 bis 18 Uhr
+            </p>
+            <br></br>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -wo
+                <br></br>
+                Technische Hochschule Ingolstadt im Raum G105
+            </p>
+            <br></br>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -wer
+                <br></br>
+                Alle THI Studenten sind herzlich eingelanden, es können bis zu 40 Personen teilnehmen. Teams sind
+                erlaubt qulaifizieren sich jedoch nicht für das gewinnen von Preisen.
+            </p>
+            <br></br>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -sponsoren
+                <br></br>
+                Alle THI Studenten sind herzlich eingelanden, es können bis zu 40 Personen teilnehmen. Teams sind
+                erlaubt qulaifizieren sich jedoch nicht für das gewinnen von Preisen.
+            </p>
+            <br></br>
+            <p>
+                user@NEULAND:~$ ./ctf-2021 -anmeldung
+                <br></br>
+                <Form method="post" action="api/submit">
+                    <Form.Group className="mb-3">
+                        <Form.Label>Name / Nickname</Form.Label>
+                        <Form.Control
+                            name="name"
+                            type="text"
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>E-Mail-Adresse:</Form.Label>
+                        <Form.Control
+                            name="email"
+                            type="email"
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Studiengang:</Form.Label>
+                        <Form.Control
+                            name="course"
+                            type="text"
+                            placeholder="Informatik 3. Semester, Bachelorand Künstliche Intelligenz, ..."
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <HCaptcha
+                            sitekey={hCaptchaSiteKey}
+                            onVerify={() => setVerified(true)}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" disabled={!verified}>Absenden</Button>
+                </Form>
+            </p>
+            <Footer/>
+        </Container>
   )
 }
 Home.propTypes = {

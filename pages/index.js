5dd941de46c4dc1aed8cdffb5e8f5b980b8ec4e5
Alexander Horn <alexhorn@users.noreply.github.com>
2021-11-15 16:39:13 +0100

    Update COVID-19 strategy

--- pages/index.js
+++ pages/index.js
@@ -56,8 +56,8 @@ export default function Home ({ hCaptchaSiteKey }) {
         <b>Place</b>: Technische Hochschule Ingolstadt in room G105<br/>
         <b>Prizes</b>: 1st place: 500€, 2nd and 3rd place: Hack the Box credits (teams are allowed, but prizes are only available
         for the best three lone fighters)<br/>
-        <b>Corona</b>: Please note that this event will be held with a 3G+ concept!
-        Make sure to bring your vaccination certificate and/or a recent PCR-Test<br/>
+        <b>Corona</b>: Please note that this event will be held with a 2G concept!
+        Make sure to bring your vaccination certificate.<br/>
       </p><br/>
 
       <p>
@@ -123,7 +123,7 @@ export default function Home ({ hCaptchaSiteKey }) {
             <Form.Check
               type="checkbox"
               name="covid19"
-              label="I am vaccinated and/or will bring a fresh PCR-Test."
+              label="I am vaccinated."
               />
           </Form.Group>
           <Form.Group>

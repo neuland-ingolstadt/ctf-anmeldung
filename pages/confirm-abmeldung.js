324724dfca53bed1703f6fae034586e5efd191fc
Jakob Löw <jakob@m4gnus.de>
2022-03-31 21:43:05 +0200

    :sparkles: add confirm-abmeldung page

--- pages/confirm-abmeldung.js
+++ pages/confirm-abmeldung.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -10,17 +9,8 @@ export default function Home () {
     <Container className={[styles.container, 'p-3']}>
 
       <Header />
-      <p>
-        <Button
-          variant="primary"
-          className="abmelden"
-          type="submit"
-          onClick={() => window.location.replace(
-            window.location.href.replace('abmeldung', 'confirm-abmeldung')
-          )}
-        >
-          I will not make it to the event :(
-        </Button>
+      <p className="abmelden">
+        Sad to see you go :(
       </p>
       <Footer/>
     </Container>

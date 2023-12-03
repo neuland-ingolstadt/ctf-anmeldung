82fee1d7eeff307d1a216fb908e97b4c37dd00e3
Manuel Voit <57528377+manuel-voit@users.noreply.github.com>
2023-12-03 21:44:51 +0100

    Remove registration

--- pages/index.js
+++ pages/index.js
@@ -121,72 +121,12 @@ export default function Home ({ hCaptchaSiteKey }) {
 
       {genTitle('registration')}
       <div>
-        You can register for the event with the following form. The number of spots is limited. We will get back to you
-        not later than 4 days before the event to confirm or decline your registration.<br/>
-        The event is intended for the students at Technische Hochschule Ingolstadt. Therefore, these registrations will
-        be accepted preferably.<br/>
-        <br/>
-        <Form method="post" action="api/submit">
-          <Form.Group className="mb-3">
-            <Form.Label>Name <sup>*</sup></Form.Label>
-            <Form.Control
-              name="name"
-              type="text"
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Email address <sup>*</sup></Form.Label>
-            <Form.Control
-              name="email"
-              type="email"
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Course of study <sup>*</sup></Form.Label>
-            <Form.Control
-              name="course"
-              type="text"
-              placeholder="Computer Science 3rd semester bachelor, ..."
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>T-Shirt size (unisex)</Form.Label>
-            <Form.Select aria-label="T-Shirt size" name="shirt">
-              <option>None selected</option>
-              <option value="XXS">XXS</option>
-              <option value="XS">XS</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
-              <option value="XXL">XXL</option>
-              <option value="3XL">3XL</option>
-              <option value="4XL">4XL</option>
-              <option value="5XL">5XL</option>
-            </Form.Select>
-          </Form.Group>
-          <br/>
           <p>
-            The above data is required for the planning and execution of the Neuland CTF. Photographs will also be taken at the
-            event. Further information on data processing can be found in the <a href="/privacy_policy.pdf" target="_blank" rel="noreferrer">privacy policy</a>.
+            The registration for the event is now closed. All available spots have been allocated.
+            Everyone who registered for the event received an email with either the acceptance or the rejection for the event.
+            Some people have been placed on the waitlist in case someone else cancels.<br/><br/>
+            Information on data processing can be found in the <a href="/privacy_policy.pdf" target="_blank" rel="noreferrer">privacy policy</a> of the event.
           </p>
-          <br/>
-          <center>
-            <Form.Group>
-              <HCaptcha
-                sitekey={hCaptchaSiteKey}
-                onVerify={() => setVerified(true)}
-              />
-            </Form.Group>
-          </center>
-          <br></br>
-          <center>
-            <Button variant="primary" type="submit" disabled={!verified} className="absend">Send</Button>
-          </center>
-        </Form>
       </div>
       <br></br>
       <Footer/>
